a87a7efd477306f9ad937727354b2b21e8d205f0
hunglem <lemanhhung0108@gmail.com>
2023-09-08 17:52:07 +0700

    Build docker-compose

--- cypress/e2e/checkdb.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('h2.subtitle').then(h2 => {
-      expect(h2.text(), 'You are connected to MongoDB');
-    })
-  })
-})
